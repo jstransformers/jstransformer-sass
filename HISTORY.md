b7e977ff8f1d6ad0e4d30b1f34add6f433479f7d
Rob Loach <robloach@gmail.com>
2015-07-19 18:38:13 -0400

    Build upon jstransformer-scss

---
+++ HISTORY.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## v0.1.0: 2015-07-19
+
+- Updated Boilerplate
+- Builds upon [`jstransformer-scss`](https://github.com/jstransformers/jstransformer-scss)
+
+## v0.0.1: 2014-11-27
+
+- Initial release
--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 'use strict';
 
-var sass = require('sass');
+var scss = require('jstransformer-scss')
 
-exports.name = 'sass';
-exports.outputFormat = 'css';
+exports.name = 'sass'
+exports.outputFormat = 'css'
 
-exports.render = function (str, options) {
-  return sass.render(str);
-};
+exports.render = function (str, options, locals) {
+  options.indentedSyntax = true
+  return scss.render(str, options, locals)
+}
+exports.renderAsync = function (str, options, locals) {
+  options.indentedSyntax = true
+  return scss.renderAsync(str, options, locals)
+}
+exports.renderFile = function (str, options, locals) {
+  options.indentedSyntax = true
+  return scss.renderFile(str, options, locals)
+}
+exports.renderFileAsync = function (str, options, locals) {
+  options.indentedSyntax = true
+  return scss.renderFileAsync(str, options, locals)
+}
